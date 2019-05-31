de6dca122371e998cf7e718e5ad01bf80bc6198a
John Cross <jwcross1@dmacc.edu>
2019-05-31 13:44:00 -0500

    first commit

---
+++ src/com/johncross/Main.java
@@ -0,0 +1,25 @@
+package com.johncross;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344); //entire line minus data type is the expression
+        int highScore = 50; //
+
+        if(highScore == 50) { //things in parentheses are an expression
+            System.out.println("This is an expression");
+        }
+
+    }
+}
+
+
+/*
+expressions:
+score = 100
+score > 99
+"You got the high score!"
+score = 0
+ */
